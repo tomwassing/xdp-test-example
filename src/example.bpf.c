abc7f6bf0fab1906bc2470876292ebbe5cf275bd
Tom J. Wassing <tjwassing@gmail.com>
2023-11-16 14:07:29 +0000

    Added simpel parsing

--- src/example.bpf.c
+++ src/example.bpf.c
@@ -1,11 +1,23 @@
-// SPDX-License-Identifier: GPL-2.0 OR BSD-3-Clause
-/* Copyright (c) 2020 Facebook */
 #include "vmlinux.h"
 #include <bpf/bpf_helpers.h>
 
-SEC("xdp")
-int xdp_prog_simple(struct xdp_md *ctx)
+SEC("xdp_drop_icmp")
+int drop_icmp(struct xdp_md *ctx)
 {
+	void *data_end = (void *)(long)ctx->data_end;
+	void *data = (void *)(long)ctx->data;
+
+
+	struct ethhdr *eth = data;
+	struct iphdr *ip = data + sizeof(*eth);
+	struct icmphdr *icmp = data + sizeof(*eth) + sizeof(*ip);
+
+	if ((void *)icmp + 1 > data_end)
+		return XDP_PASS;
+
+	if (ip->protocol == IPPROTO_ICMP)
+		return XDP_DROP;
+
 	return XDP_PASS;
 }
 
--- src/test.c
+++ src/test.c
@@ -1,15 +1,45 @@
 #include <bpf/bpf.h>
 #include <assert.h>
+#include <netinet/ip.h>
+#include <linux/icmp.h>
+#include <net/ethernet.h>
+#include <arpa/inet.h>
 
-#include "net/ethernet.h"
-#include "linux/ip.h"
-#include "netinet/tcp.h"
 #include "example.skel.h"
 
 int main (int argc, char *argv[]) {
 
         // Mock packet.
-        char pkt[(sizeof(struct ethhdr) + sizeof(struct iphdr) + sizeof(struct tcphdr))];
+        int pkt_len = sizeof(struct ethhdr) + sizeof(struct iphdr) + sizeof(struct icmphdr);
+        char pkt[pkt_len];
+
+        // Fill in Ethernet header.
+        struct ethhdr *eth_hdr = (struct ethhdr*) pkt;
+        unsigned char dest_mac[ETH_ALEN] = {0x01, 0x23, 0x45, 0x67, 0x89, 0xAB};
+        unsigned char src_mac[ETH_ALEN] = {0xAB, 0x89, 0x67, 0x45, 0x23, 0x01};
+        memcpy(eth_hdr->h_dest, dest_mac, ETH_ALEN);
+        memcpy(eth_hdr->h_source, src_mac, ETH_ALEN);
+        eth_hdr->h_proto = htons(ETH_P_IP);
+
+        // Fill in IP header.
+        struct iphdr * ip_hdr = (struct iphdr*) (pkt + sizeof(struct ethhdr));
+        ip_hdr->version = 4;
+        ip_hdr->ihl = 5;
+        ip_hdr->tos = 0;
+        ip_hdr->tot_len = htons(pkt_len);
+        ip_hdr->id = htons(42);
+        ip_hdr->frag_off = 0;
+        ip_hdr->ttl = 64;
+        ip_hdr->protocol = IPPROTO_ICMP;
+        ip_hdr->check = 0;
+        ip_hdr->saddr = inet_addr("");
+        ip_hdr->daddr = inet_addr("");
+
+        // Fill in ICMP header.
+        struct icmphdr *icmp_hdr = (struct icmphdr*) (pkt + sizeof(struct ethhdr) + sizeof(struct iphdr));
+        icmp_hdr->type = ICMP_ECHO;
+        icmp_hdr->code = 0;
+        icmp_hdr->checksum = 0;
 
         // Define our BPF_PROG_RUN options with our mock data.
         struct bpf_test_run_opts opts = {
@@ -25,8 +55,10 @@ int main (int argc, char *argv[]) {
                 return -1;
         }
 
-        // Get the prog_fd from the skeleton, and run our test.
-        int prog_fd = bpf_program__fd(prog->progs.xdp_prog_simple);
+        // Get the prog_fd from the skeleton.
+        int prog_fd = bpf_program__fd(prog->progs.drop_icmp);
+        
+        // Run test with ICMP packet.
         int err = bpf_prog_test_run_opts(prog_fd, &opts);
         if (err != 0) {
                 printf("[error]: bpf_prog_test_run_opts failed: %d\n", err);
@@ -35,8 +67,21 @@ int main (int argc, char *argv[]) {
         }
 
         // Testing!
+        assert(opts.retval == XDP_DROP);
+        printf("[success] drop ICMP packets\n");
+
+        // Run test with non-ICMP packet.
+        ip_hdr->protocol = IPPROTO_TCP;
+        err = bpf_prog_test_run_opts(prog_fd, &opts);
+        if (err != 0) {
+                printf("[error]: bpf_prog_test_run_opts failed: %d\n", err);
+                perror("bpf_prog_test_run_opts");
+                return -1;
+        }
+
+        // Testing!
         assert(opts.retval == XDP_PASS);
-        printf("[success]\n");
+        printf("[success] pass non-ICMP packets\n");
 
         return 0;
 }
